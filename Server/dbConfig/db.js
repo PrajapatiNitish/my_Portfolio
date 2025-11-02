7d5d974e1073c33f2a349c5f06bc8fe0a951dfdd
Nitish Prajapati <130295260+PrajapatiNitish@users.noreply.github.com>
2025-11-02 20:04:02 +0530

    recorrect db.js connection so that it use remotely
    Updated database configuration to use DB_NAME and added SSL support.

--- Server/dbConfig/db.js
+++ Server/dbConfig/db.js
@@ -1,5 +1,5 @@
 //Entry point of database
-
+import fs from "fs"
 import dotenv from "dotenv";
 import mysql from 'mysql2';
 
@@ -10,14 +10,20 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_SSL_CA),
+    rejectUnauthorized: true, //enable ssl
+  },
+
   waitForConnections: true,
 });
 
 // Find any error related to database
 connection.connect(error => {
     if(error) {
-        console.log("Error : ", error);
+        console.log("db Error : ", error);
     }else {
         console.log("Db is running...")
     }
