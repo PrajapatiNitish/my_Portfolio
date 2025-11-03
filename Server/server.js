bb7ed343d2f24c41d5236086e683b2624ba8eb4b
PrajapatiNitish <(valid-prajapatinitish49@gmail.com)>
2025-11-03 19:04:12 +0530

    03M11O25N_12A28, fixes cors policy issue for my domain

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,6 @@ import express from "express"; //Data is comming in asynchronise. for synchronis
 import dotenv from "dotenv";
 import cors from "cors"; //Resolve the cors policy error
 
-
 //To use process.env file
 dotenv.config();
 
@@ -13,18 +12,32 @@ const app = express();
 // Server is ready. Entry Point
 const port = process.env.PORT; //This port will be client_url or public url or domain that we are going to buy.
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, //need to add domain url
-  methods: ['POST'],
-  credentials: true
-})); //Allow other link using cors policy. CORS - Cross origine requests.
+const allowedOrigins = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL.split(",").map((origin) => {
+      origin.trim();
+    })
+  : [];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); //Allow frontend server through cros origin policy
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    }, //need to add domain url
+    methods: ["POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+); //Allow other link using cors policy. CORS - Cross origine requests.
 app.use(express.json()); //In case if we need to send json data.
 app.use(express.urlencoded({ extended: true })); //Express can understand json fromate data.
 
 // get feedback of user from client
 import feedbackRoute from "./routes/feedbackRoute.js";
 app.use("/home-page", feedbackRoute);
-
 
 // App listening
 app.listen(port, () => {
